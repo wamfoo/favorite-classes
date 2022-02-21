bb417c7958dea2c17ee30718a65e536b7875c299
William Mejia <williammejia4u4@gmail.com>
2022-02-21 13:52:12 -0800

    First commit

---
+++ src/components/Class/ClassGraphQL.js
@@ -0,0 +1,59 @@
+import "./Class.css";
+import React, {useState, useEffect} from 'react';
+
+function ClassGraphQL(props) {
+
+    const [classInfo, setClassInfo] = useState({});
+
+    const url = "https://api.peterportal.org/graphql"
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const query = `
+                query {
+                    course(id:"${props.name}") {
+                        title
+                        department_name
+                        description
+                    }
+                }
+            `
+
+            const response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify({query}),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const data = await response.json();
+            console.log(data);
+            setClassInfo(data.data.course);
+
+        }
+        fetchData();
+    }, [props.name])
+    let info;
+    if (classInfo) {
+        info = <div className="information">
+            <p id="title"> {classInfo.title} </p>
+            <p id="department"> {classInfo.department_name} </p>
+            <p id="description"> {classInfo.description} </p>
+        </div>
+    } else if (classInfo == null) {
+        info = <p>Class Not Found</p>
+    } else {
+        info = <p>Loading...</p>
+    }
+
+    return (
+        <div className="class">
+            {props.name}
+            <div>
+                {info}
+            </div>
+        </div>
+    )
+}
+
+export default ClassGraphQL;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import Class from "../Class/Class";
+import ClassGraphQL from "../Class/ClassGraphQL";
+import "./Home.css"
+
+function Home(props) {
+
+    const [value, setValue] = useState('');
+    const [favoriteClasses, setClasses] = useState([]);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!favoriteClasses.includes(value)) {
+            setClasses(favoriteClasses.concat(value));
+            setValue('');
+        }
+        console.log(favoriteClasses);
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Add Favorite Class</label>
+                <input type="text" value={value} onChange={handleChange}></input>
+                <button type="submit">Add class!</button>
+            </form>
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) => 
+                    <Class name={favClass} key={favClass}></Class>
+                )}
+            </div>
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) => 
+                    <ClassGraphQL name={favClass} key={favClass}></ClassGraphQL>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
